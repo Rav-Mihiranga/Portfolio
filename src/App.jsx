02bdcf3cba451b03b72026aed7032dcc4ec7ea97
Rav-Mihiranga <ravindumw99@gmail.com>
2025-11-23 00:18:40 +0530

    updated 04

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Intro from "./components/Intro/intro";
 import About from "./components/About/About";
 import Services from "./components/Services/services";
 import MyWork from "./components/MyWork/mywork";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
         <About />
         <Services />
         <MyWork />
+        <Contact />
+        <Footer />
         
       </div>
     
